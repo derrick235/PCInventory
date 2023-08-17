37210a489912baabe0cf64594c6aafe1404478b7
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2023-08-16 22:08:13 -0400

    Create PC struct model for Firestore
    Create PC struct that includes id, firstName, lastName, pcName, pcType,
    pcLocation, status, isReplaced, and replacedBy.

---
+++ PCInventory/Structs/PC.swift
@@ -0,0 +1,61 @@
+//
+//  PC.swift
+//  PCInventory
+//
+//  Created by Derrick Lu on 8/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct PC: Identifiable, Codable {
+    
+    var id: String
+    var firstName: String
+    var lastName: String
+    var isReplaced: Bool
+    var pcLocation: String
+    var pcSN: String
+    var pcType: String
+    var replacedBy: String
+    var status: String
+    
+    func statusIcon() -> String {
+        
+        if self.status == "Active" {
+            return "checkmark.circle.fill"
+        }
+        else if self.status == "Pending Replace" || self.status == "Pending Use" {
+            return "arrow.left.arrow.right.circle.fill"
+        }
+        else {
+            return "x.circle.fill"
+        }
+        
+    }
+    
+    func statusColor() -> Color {
+        
+        if self.status == "Active" {
+            return Color.green
+        }
+        else if self.status == "Pending Replace" || self.status == "Pending Use" {
+            return Color.yellow
+        }
+        else {
+            return Color.red
+        }
+        
+    }
+    
+    func replaceString() -> String {
+        
+        if isReplaced {
+            return "Yes"
+        }
+        return "No"
+        
+    }
+    
+}
+
